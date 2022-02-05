729bdb96a4b7f158482f2bf35aa39a45e347f3c7
Jclan01 <94146223+Jclan01@users.noreply.github.com>
2022-02-05 16:37:37 -0500

    Add files via upload

---
+++ flowerGame.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+    var name = prompt("What is your adventurer name?");
+    var title = document.getElementById("title");
+    title.innerHTML = name + " The Adventurer";
+}
+
+
+var adventurer = document.getElementById("adventurer");
+
+//type of problem: 0 is basic addition, 1 is basic multiplication, 2 is derivatives, 3 is integrals
+function womantime() {
+    var optionArr = new Array(5);
+    var type = 0;
+    var valA = 0;
+    var valB = 0;
+    var question;
+    type = Math.floor(Math.random * 4);
+    if (type == 0) {
+        valA = -100 + Math.floor(Math.random * 201);
+        valB = -100 + Math.floor(Math.random * 201);
+        question = valA + " + " + valB;
+        indexCorr = Math.floor(Math.random * 5);
+        optionArr[indexCorr] = valA + valB;
+        for (let i = 0; i < optionArr.length; i++) {
+            if (i != indexCorr) {
+                optionArr[i] = optionArr[indexCorr] + -100 + Math.floor(Math.random * 101);
+            }
+        }
+    } else if (type == 1) {
+        valA = -10 + Math.floor(Math.random * 21);
+        valB = -10 + Math.floor(Math.random * 21);
+        question = valA + " * " + valB;
+    } else if (type == 2) {
+        valA = 1 + Math.floor(Math.random * 10);
+        valB = 1 + Math.floor(Math.random * 20);
+        question = "Derivative of 1/" + valB + "*x^" + valA;
+    } else {
+        valA = 1 + Math.floor(Math.random * 10);
+        valB = 1 + Math.floor(Math.random * 20);
+        question = "Integral of 1/" + valB + "*x^" + valA;
+    }
+
+    var option1 = document.getElementById("option1");
+    option1.innerHTML = optionArr[0];
+    var option2 = document.getElementById("option2");
+    option2.innerHTML = optionArr[1];
+    var option3 = document.getElementById("option3");
+    option3.innerHTML = optionArr[2];
+    var option4 = document.getElementById("option4");
+    option4.innerHTML = optionArr[3];
+    var option5 = document.getElementById("option5");
+    option5.innerHTML = optionArr[4];
+
+}
